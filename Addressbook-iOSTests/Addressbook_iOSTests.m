d2f1ba18898888ed9a510706aa8a6e2720fccbda
Superbil <superbil@gmail.com>
2014-04-21 22:51:44 +0800

    unit test on fetch organizations and persons
    * this is a example for fetch two different data model

--- Addressbook-iOSTests/Addressbook_iOSTests.m
+++ Addressbook-iOSTests/Addressbook_iOSTests.m
@@ -25,12 +25,9 @@
     [super tearDown];
 }
 
-- (void)checkResultWithTask:(BFTask *)task
+- (void)checkResult:(id)result
 {
-    XCTAssertNil(task.error, @"There should not be any error.");
-    XCTAssertNotNil(task.result, @"There should had result.");
-
-    NSArray *popoloOrgs = task.result;
+    NSArray *popoloOrgs = result;
     NSLog(@"result.count: %lu", popoloOrgs.count);
 
     for (NSDictionary *onePopolo in popoloOrgs) {
@@ -44,6 +41,14 @@
 
         XCTAssertNotNil([onePopolo valueForKeyPath:@"id"], @"This should had id.");
     }
+}
+
+- (void)checkResultWithTask:(BFTask *)task
+{
+    XCTAssertNil(task.error, @"There should not be any error.");
+    XCTAssertNotNil(task.result, @"There should had result.");
+
+    [self checkResult:task.result];
 }
 
 - (void)testOrgs
@@ -84,6 +89,44 @@
 
     [[[G0VAddressbookClient sharedClient] fetchPersonsWithMatchingString:matchesString] continueWithBlock:^id(BFTask *task) {
         looping = NO;
+        [self checkResultWithTask:task];
+        return nil;
+    }];
+
+    while (looping) {
+		[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.2]];
+	}
+}
+
+- (void)testOrgsAndPersonsWithMatchesString
+{
+    NSString *matchesString = @"張";
+    __block BOOL looping = YES;
+    NSMutableArray *results = [NSMutableArray array];
+
+    [[[[G0VAddressbookClient sharedClient] fetchOrganizationsWithMatchesString:matchesString] continueWithBlock:^id(BFTask *task) {
+        if (task.result) {
+            // append to |results|
+            [results addObjectsFromArray:task.result];
+
+            return [[G0VAddressbookClient sharedClient] fetchPersonsWithMatchingString:matchesString];
+        }
+
+        // no |task.result| should had error
+        [self checkResultWithTask:task];
+        return nil;
+    }] continueWithBlock:^id(BFTask *task) {
+        looping = NO;
+
+        if (task.result) {
+            // append to |results|
+            [results addObjectsFromArray:task.result];
+
+            [self checkResult:results];
+            return nil;
+        }
+
+        // no |task.result| should had error
         [self checkResultWithTask:task];
         return nil;
     }];
