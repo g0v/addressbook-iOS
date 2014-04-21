a1f24a124b647b931255cbce5c62019391afa363
Superbil <superbil@gmail.com>
2014-04-22 03:12:10 +0800

    implemented PopoloLink

--- Classes/PopoloLink.h
+++ Classes/PopoloLink.h
@@ -7,6 +7,26 @@
 
 #import "JSONModel.h"
 
+@protocol PopoloLink
+@end
+
+/**
+ Link
+
+ A URL
+
+ JSON Schema: http://json-schema.org/draft-03/schema
+ */
+
 @interface PopoloLink : JSONModel
+
+/**
+ A URL
+ */
+@property (nonatomic, strong) NSString *url;
+/**
+ A note, e.g. 'Wikipedia page'
+ */
+@property (nonatomic, strong) NSString<Optional> *note;
 
 @end
